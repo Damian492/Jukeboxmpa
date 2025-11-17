467a5de8772132c8fa38acd768f92976669f575e
Damian <99075982@mydavinci.nl>
2025-11-17 10:13:57 +0100

    Add project files.

---
+++ Jukeboxmpa/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Jukeboxmpa.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
