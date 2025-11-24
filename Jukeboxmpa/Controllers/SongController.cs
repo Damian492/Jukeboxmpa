6b49ffafc6eb68e525c13d2fb6bc27fb558fe0aa
Damian <99075982@mydavinci.nl>
2025-11-24 11:39:34 +0100

    error fix

--- Jukeboxmpa/Controllers/SongController.cs
+++ Jukeboxmpa/Controllers/SongController.cs
@@ -1,33 +1,54 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Jukeboxmpa.Data;
+﻿using Jukeboxmpa.Data;
 using Jukeboxmpa.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Jukeboxmpa.Controllers
 {
+    // De Controller handel alle verzoeken mte song 
     public class SongController : Controller
     {
         private readonly ApplicationDbContext _context;
 
-        public SongController(ApplicationDbContext context) // Constructor Injectie
+        // Constructor toegang met database krijgn
+        public SongController(ApplicationDbContext context)
         {
             _context = context;
         }
 
-        // GET: Song/Index
+        //alle liedjes
+        // GET: /Song/Index
         public async Task<IActionResult> Index()
         {
-            // Haal alle liedjes uit de 'Songs' tabel op en stuur ze naar de View
+            // Haalt alle liedjesop uit de database
+            // .Include() artiesten en albums tonen
             return View(await _context.Songs.ToListAsync());
         }
 
-        // GET: Song/Create
+        // 2. CREATE (GET): song toevoegne
+        // GET: /Song/Create
         public IActionResult Create()
         {
-            //een nieuw song in te voeren
             return View();
         }
 
-        // POST: Song/Create
+        // 3. CREATE (POST): 
+        // POST: /Song/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public async Task<IActionResult> Create([Bind("Title,Artist,Album,FilePath")] Song song)
+        {
+
+
+            if (ModelState.IsValid)
+            {
+                _context.Add(song);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(song);
+        }
     }
 }
--- Jukeboxmpa/Program.cs
+++ Jukeboxmpa/Program.cs
@@ -8,14 +8,17 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddAuthorization();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    // https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -23,7 +26,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseAuthentication(); // if authentication is configured
 app.UseAuthorization();
 
 app.MapControllerRoute(
